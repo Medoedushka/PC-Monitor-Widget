78a8f7db39e70311b189e3ce963ea80db2896705
Medoedushka <knife491@gmail.com>
2019-08-19 22:54:01 +0300

    Р РµРґР°РєС‚РёСЂРѕРІР°РЅРёРµ

--- PC Monitor Widget/PC Monitor Controls/CPUTemp.cs
+++ PC Monitor Widget/PC Monitor Controls/CPUTemp.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Management;
 
 namespace PC_Monitor_Widget
 {
@@ -29,9 +30,24 @@ namespace PC_Monitor_Widget
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            List<Temperature> tempList = Temperature.Temperatures;
-            cpb_CPUTemp.Text = string.Format("{0}°С", tempList[0].CurrentValue);
-            cpb_CPUTemp.SetValue((int)tempList[0].CurrentValue);
+            
+            try
+            {
+                Double CPUtprt = 0;
+                System.Management.ManagementObjectSearcher MOS = new System.Management.ManagementObjectSearcher("root\\WMI",
+                        "SELECT * FROM MSAcpi_ThermalZoneTemperature");
+                foreach (System.Management.ManagementObject Mo in MOS.Get())
+                {
+                    CPUtprt = Convert.ToDouble(Convert.ToDouble(Mo.GetPropertyValue("CurrentTemperature".ToString())) - 2732) / 10.0;
+                    cpb_CPUTemp.Text = string.Format("{0}°С", (int)CPUtprt);
+                    cpb_CPUTemp.SetValue((int)CPUtprt);
+                    break;
+                }
+            }
+            catch (ManagementException ex)
+            {
+                MessageBox.Show("Ошибка получения данных " + ex.Message);
+            }
         }
     }
 }
